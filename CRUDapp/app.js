1b030bbae1c77a97c6850c7d8d1bfb001b0097a2
SaiKUndurthi <sai.undurthi@gmail.com>
2017-11-16 19:19:34 -0800

    Started a simple server.

---
+++ CRUDapp/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+app.get('/', (req,res)=>{
+  res.send("Hello World");
+});
+
+app.listen(PORT, ()=>{
+  console.log("Server started on PORT ", PORT);
+});
