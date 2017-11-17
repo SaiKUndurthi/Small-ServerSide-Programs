512cf481d0acb579171f614984f013141db89c96
SaiKUndurthi <sai.undurthi@gmail.com>
2017-11-16 19:57:46 -0800

    Added pug view engine.

--- CRUDapp/app.js
+++ CRUDapp/app.js
@@ -1,8 +1,45 @@
 const express = require('express');
+const path  = require('path');
+//Init app
 const app = express();
+
+
 const PORT = 3000;
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+
+//Home Route
 app.get('/', (req,res)=>{
-  res.send("Hello World");
+  let articles = [
+    {
+      id:1,
+      title: 'First Article',
+      author: 'SKU'
+    },
+    {
+      id:2,
+      title: 'Second Article',
+      author: 'Sai'
+    },
+    {
+      id:3,
+      title: 'Third Article',
+      author: 'Sai Krishna Undurthi'
+    }
+];
+  res.render("index", {
+    title:'ARTICLES',
+    articles: articles
+  });
+});
+
+//Add Article Route
+app.get('/articles/add', (req, res)=>{
+  res.render('add_articles', {
+    title:'Add Article'
+  });
 });
 
 app.listen(PORT, ()=>{
