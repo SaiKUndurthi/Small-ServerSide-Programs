af5cb27fa3173eb4a19533276b00ed2206c6ef3c
SaiKUndurthi <sai.undurthi@gmail.com>
2017-11-18 23:40:37 -0800

    Update & Delete Articles.

--- CRUDapp/app.js
+++ CRUDapp/app.js
@@ -102,6 +102,48 @@ app.post('/articles/add', (req, res)=>{
   })
 })
 
+
+//Load Edit form
+app.get('/article/edit/:id', (req, res)=>{
+  Article.findById(req.params.id, (err, article)=>{
+    res.render('edit_article', {
+      title:'Edit Article',
+      article: article
+    });
+  });
+});
+
+//Update submit POST
+app.post('/articles/edit/:id', (req, res)=>{
+  let article = {};
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  let query = {_id:req.params.id}
+
+  Article.update(query, article, (err)=>{
+    if(err){
+      console.log(err);
+      return;
+    }else{
+      res.redirect('/');
+    }
+  });
+});
+
+
+app.delete('/article/:id', (req, res)=>{
+  let query = {_id:req.params.id}
+
+  Article.remove(query, (err)=>{
+    if(err){
+      console.log(err);
+    }
+    res.send('Success');
+  });
+});
+
 app.listen(PORT, ()=>{
   console.log("Server started on PORT ", PORT);
 });
