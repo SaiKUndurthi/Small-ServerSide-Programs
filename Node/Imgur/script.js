5b3c9a8778fd07672fd514be9029ebd07c128d5f
SaiKUndurthi <sai.undurthi@gmail.com>
2018-02-11 23:44:00 -0800

    Fetching image from IMGUR API

---
+++ Node/Imgur/script.js
@@ -0,0 +1,43 @@
+var clientID = key.CLIENT_ID;
+const request = new Request('https://api.imgur.com/3/gallery/image/pmMAAr6', {
+        headers: new Headers({
+            'Authorization': 'Client-ID '+clientID
+        })
+    });
+
+    function createImageElement(data) {
+        var pT = document.createElement('IMG');
+          pT.setAttribute('src', data);
+          pT.setAttribute('width', '80');
+          pT.setAttribute('height', '80');
+
+        document.getElementById("three").appendChild(pT);
+      }
+
+
+
+
+    document.getElementById('one').addEventListener('click', function(e){
+        alert('Div 1');
+        e.stopPropagation();
+    })
+
+    document.getElementById('two').addEventListener('click', function(e){
+        alert('Div 2');
+        fetch('')
+        e.stopPropagation();
+    })
+
+    document.getElementById('three').addEventListener('click', function(e){
+        alert('Fetching a picture using IMGUR API');
+        fetch(request).then(function(res){
+            //console.log(res);
+           return res.json();
+        }).then(function(src){
+            //console.log(src);
+            createImageElement(src.data.link);
+        })
+        e.stopPropagation();
+    })
+
+
